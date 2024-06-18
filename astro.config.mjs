1d041735a9b982b5a6b2ffc367cbf3ba3b12d0d3
Alessandro Cipriani <social@magiaslab.com>
2024-06-18 18:12:07 +0200

    pacchetti

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+import robotsTxt from "astro-robots-txt";
+
+import sitemap from "astro-sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), robotsTxt(), sitemap()]
 });
