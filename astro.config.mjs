1570c457fc2ed04e94ee6ea02c818c7f8ac66dcf
Alessandro Cipriani <social@magiaslab.com>
2024-07-20 09:35:15 +0200

    ok partito

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import robotsTxt from "astro-robots-txt";
 import sitemap from "astro-sitemap";
+import astroI18next from "astro-i18next";
+
 
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), robotsTxt(), sitemap(), icon()]
+  integrations: [tailwind(), robotsTxt(), sitemap(), icon(), astroI18next()],
 });
