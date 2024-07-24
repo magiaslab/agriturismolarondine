f8726281483c00d36f2396f9c672303a4d6517f7
magiaslab <social@magiaslab.com>
2024-07-24 23:00:48 +0200

    slider

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,11 @@ import tailwind from "@astrojs/tailwind";
 import robotsTxt from "astro-robots-txt";
 import sitemap from "astro-sitemap";
 import astroI18next from "astro-i18next";
-
-
 import icon from "astro-icon";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), robotsTxt(), sitemap(), icon(), astroI18next()],
+  integrations: [tailwind(), robotsTxt(), sitemap(), icon(), astroI18next(), react()]
 });
---
+++ src/components/sliderHome.jsx
@@ -0,0 +1,47 @@
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+
+
+// import required modules
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+
+export default function App() {
+  return (
+    <>
+      <Swiper
+      
+        spaceBetween={30}
+        effect={'fade'}
+        centeredSlides={true}
+        autoplay={{
+          delay: 8000,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={false}
+        modules={[Autoplay, Pagination, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide><img src="/img/image00001.jpeg" alt="" /></SwiperSlide>
+        <SwiperSlide><h1 class="text-5xl mb-8 lg:text-7xl font-sans font-black subpixel-antialiased text-primary uppercase">Titolo della slide</h1>
+        </SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
+    </>
+  );
+}
