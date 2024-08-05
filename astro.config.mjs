f39e2ce30f297e3513d36c52e34ca2d12284c3b6
Alessandro Cipriani <social@magiaslab.com>
2024-08-05 21:41:09 +0200

    prova

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,6 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), robotsTxt(), sitemap(), icon(), astroI18next(), react()],
+  integrations: [tailwind(), robotsTxt(), sitemap(), icon(), react(), astroI18next()],
 
 });
